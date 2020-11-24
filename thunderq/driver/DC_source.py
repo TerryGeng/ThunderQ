90088e24ec06af8b175a5a6125355683828ac91a
Terry Geng <gengyanda@gmail.com>
2020-11-24 16:45:19 +0800

    chore: removed old drivers.

--- thunderq/driver/DC_source.py
+++
@@ -1,8 +0,0 @@
-class DCChannel:
-    def __init__(self, _name, _rc, _ch):
-        self.rc = _rc
-        self.name = _name
-        self.channel = _ch
-
-    def set_offset(self, offset):
-        self.rc.setValue('Offset', ch=self.channel, value=offset)
