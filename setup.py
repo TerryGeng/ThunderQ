45f0636c12a57eb20c331b1cf9bf582f248986f7
Terry Geng <gengyanda@gmail.com>
2020-12-23 15:35:25 +0800

    fix: find_pacakges failed in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="ThunderQ",
     version="0.1",
-    packages=find_packages(include=['thunderq', 'thunderq.*']),
+    packages=[
+        'thunderq',
+        'thunderq.helper',
+        'thunderq.sequencer',
+        'thunderq.experiment',
+        'thunderq.procedures.native',
+        'thunderq.cycles.native',
+        'thunderq.waveforms.native',
+    ],
 
     author="Yanda Geng",
     author_email="gengyanda16@smail.nju.edu.cn",
